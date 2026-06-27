a363f78109af5250adef2e8e1964601d20a37ec2
SereneTide <158478012+SereneTide@users.noreply.github.com>
2026-06-27 16:07:42 +0800

    Delete E1 directory

--- E1/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# @FileName  :main
-# @Author    :SereneTide
-"""入口兼容文件 —— 所有逻辑统一在 run.py 中维护，main.py 仅做转发。"""
-from run import main
-
-if __name__ == '__main__':
-    main()
